74bb5a1beeda690dd85beec9ff342e0c59e71825
Murilo Glasser <murilo.glasser@hotmail.com>
2024-02-28 18:31:00 -0300

    Added initial fade

--- modal.js
+++ modal.js
@@ -1,9 +1,32 @@
+var timeoutEnterId;
+
+
+
 // Change image with mouse hover to gif
 function changeImage(projectId, imageUrl) {
-    var projectElement = document.getElementById(projectId);
-    if (projectElement) {
-        projectElement.src = imageUrl;
-    }
+
+    timeoutEnterId = setTimeout(function () {
+
+        var projectElement = document.getElementById(projectId);
+        if (projectElement) {
+            projectElement.src = imageUrl;
+        }
+    }, 500);
+
+}
+
+// Change image with mouse hover to gif
+function returnImage(projectId, imageUrl) {
+
+    clearTimeout(timeoutEnterId);
+    setTimeout(function () {
+
+        var projectElement = document.getElementById(projectId);
+        if (projectElement) {
+            projectElement.src = imageUrl;
+        }
+    }, 200);
+
 }
 
 // Open project modal
---
+++ overlay.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Aguarde 1 segundo (1000 milissegundos)
+
+    setTimeout(function () {
+        // Defina a opacidade do overlay para 0 (fade out)
+        document.getElementById("overlay").style.opacity = 1;
+
+        
+      
+    }, 1000);
+});
